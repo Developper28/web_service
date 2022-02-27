85565e4919de33d2f8fa9d5315d98fdf4aa4aa3e
Aicha Garci <garci01@ads.uni-passau.de>
2022-02-27 22:23:52 +0100

    Get the project name

--- webservice.py
+++ webservice.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import re
 import subprocess
+import json
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -16,19 +17,27 @@ class Server(BaseHTTPRequestHandler):
                         name_str = param["value"]
 
             message = " ".join(re.findall('[A-Z][^A-Z]*', name_str))
-                    
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(message.encode(encoding="utf-8"))
 
         elif endpoint == "/versionz":
-            sbrs = subprocess.Popen("git rev-parse HEAD", shell=True, stdout=subprocess.PIPE)
-            subprocess_return = sbrs.stdout.read()
-            subprocess_return = subprocess_return.decode("utf-8")
-            print(subprocess_return)
-            message = subprocess_return
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(message, "utf-8"))
+            git_hash = Server.execute_cmd("git rev-parse HEAD")
+            project_path = Server.execute_cmd("pwd")
+            project_name = project_path.split("/")[-1]
+            data = {"project_name": project_name, "git_hash": git_hash}
+            json_str = json.dumps(data)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json_str.encode(encoding="utf-8"))
 
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("Content not found".encode(encoding="utf-8"))
 
     @staticmethod
     def process_query_path(url_path):
@@ -43,6 +52,15 @@ class Server(BaseHTTPRequestHandler):
                 key, value = param.split("=")
                 ans["params"].append({"key": key, "value": value})
         return ans
+
+    @staticmethod
+    def execute_cmd(cmd):
+            sbrs = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+            subprocess_return = sbrs.stdout.read()
+            subprocess_return = subprocess_return.decode("utf-8")
+            ans = subprocess_return[:-1] # To remove /n
+            return ans
+
 
 # Global Variable
 hostname = "localhost"
